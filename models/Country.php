f080156328f52cb6465028bf663147866469107e
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2018-04-03 18:28:37 -0400

    updated countries table to be editable , finished email/contact support

--- models/Country.php
+++ models/Country.php
@@ -2,20 +2,48 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Country extends ActiveRecord
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property int $population
+ */
+class Country extends \yii\db\ActiveRecord
 {
-	// get all rows from the country table and order them by "name"
-//$countries = Country::find()->orderBy('name')->all();
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
 
-// get the row whose primary key is "US"
-//$country = Country::findOne('US');
-//
-//// displays "United States"
-//echo $country->name;
-//
-//// modifies the country name to be "U.S.A." and save it to database
-//$country->name = 'U.S.A.';
-//$country->save();
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['population'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 52],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
