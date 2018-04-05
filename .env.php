774e6ad2060a58c34f383ef883dfc4cb90a36695
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2018-04-05 18:16:27 -0400

    add stupid envs or now must delete as soon as it works

---
+++ .env.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'DB_USER' => 'joe',
+    'DB_PASSWORD' => 'secret',
+];
---
+++ config/.env.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'DB_USER' => 'joe',
+    'DB_PASSWORD' => 'password123',
+	   'DB_HOST' => 'mysql:host=yii2basic.c9odwixmfpp7.us-east-1.rds.amazonaws.com;port=3306;dbname=yii2basic;charset=utf8'
+];
